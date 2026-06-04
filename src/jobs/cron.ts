05d4ac87aabe281f4ef4d61bd5bd2d24c5d786a3
MarioVio1 <gattom22@gmail.com>
2026-06-04 13:47:29 +0200

    Add files via upload

---
+++ src/jobs/cron.ts
@@ -0,0 +1,14 @@
+import cron from 'node-cron';
+import { logger } from '../utils/logger.js';
+import { syncAll } from '../services/syncService.js';
+export function startCron() {
+  cron.schedule('*/30 * * * *', async () => {
+    try {
+      logger.info('Starting scheduled sync');
+      await syncAll();
+      logger.info('Scheduled sync completed');
+    } catch (error) {
+      logger.error({ error }, 'Scheduled sync failed');
+    }
+  });
+}
