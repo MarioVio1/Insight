59acae53ba36aa42ebe2f3cad8c7f38b0334ec5b
MarioVio1 <gattom22@gmail.com>
2026-06-04 14:50:45 +0200

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
-import ws from 'ws';
+import WebSocket from 'ws';
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
     realtime: {
-      transport: ws as any
+      transport: WebSocket as any
     }
   }
 );
